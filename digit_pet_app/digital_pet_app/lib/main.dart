68411c6dbab3a693b5f243f4543e766183d73be3
Steven Tran <steventran@Stevens-Air.wifi.gsu.edu>
2025-02-05 14:29:24 -0500

    Digital

---
+++ digit_pet_app/digital_pet_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const WeatherPage(),
+    );
+  }
+}
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Weather App')),
+      body: const Center(child: Text('This is Steven and Bhavya.')),
+    );
+  }
+}
